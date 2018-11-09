04abdc97843145af3661f852f202c3ac25d6e04a
pedholiveira <ped.holiveira@gmail.com>
2018-11-09 00:03:06 -0200

    Initial commit

---
+++ app/src/main/java/com/infnet/gec/app_monitoramento/MainActivity.java
@@ -0,0 +1,13 @@
+package com.infnet.gec.app_monitoramento;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
